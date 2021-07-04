e1758fdbbd48f02c069acb160637008fef1f33e6
Silent <hamidreza.neo@gmail.com>
2021-07-04 19:01:18 +0430

    Update content.js

--- src/content.js
+++ src/content.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react"
 import { Route, Switch } from "react-router-dom";
+import Projects from "./content/projects"
+import Skills from "./content/skills"
 
 class Content extends Component {
     render() {
         return (
             <div id="content">
                 <Switch>
-                    <Route path='/' component={() => <h1>Projects</h1>} exact/>
-                    <Route path='/skills' component={() => <h1>Skills</h1>} exact/>
+                    <Route path='/skills' component={Skills}/>
+                    <Route path='/' component={Projects} exact/>
                     <Route path='*' component={() => <h1 style={{marginLeft: 20}}>404 - Page Not Found</h1>}/>
                 </Switch>
             </div>
