b47718e90fd36daf560300f6159b6dd440c29ce0
Silent <hamidreza.neo@gmail.com>
2021-07-04 13:09:20 +0430

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Content from './content'
+import Menu from './menu'
+import './scss/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Menu />
+    <Content />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Menu extends Component {
+    render() {
+        return (
+            <div id="menu">
+                <div className="item">Projects</div>
+                <div className="item">Skills</div>
+            </div>
+        )
+    }
+}
+
+export default Menu
