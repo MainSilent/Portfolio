b760476c9828f9c7973b0a3c07b9a6ab2a42a3b0
Silent <hamidreza.neo@gmail.com>
2021-07-05 23:01:29 +0430

    project name

--- src/content/projects.js
+++ src/content/projects.js
@@ -7,6 +7,7 @@ class Projects extends Component {
                 {Array(10).fill().map(() => 
                 <div className="project">
                     <img className="thumbnail" src="https://camo.githubusercontent.com/12b2dde6beed6ef1073ea61434cbdf129a7a459b69ce919aad1b8295ee728368/68747470733a2f2f626573682d6b616e2e6769746875622e696f2f686f6d652f696d616765732f6261636b67726f756e642e706e67" />
+                    <p className="name">Besh-kan</p>
                     <ul>
                         <li><img src={process.env.PUBLIC_URL + `/static/logo/php.png`}/>PHP</li>
                         <li><img src={process.env.PUBLIC_URL + `/static/logo/html.png`}/>HTML</li>
