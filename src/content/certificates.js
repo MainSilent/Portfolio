281c1e1a4f435383b050ace50b8ab39c838768a0
MainSilent <70273681+MainSilent@users.noreply.github.com>
2022-01-29 17:11:53 +0330

    add certificates style

--- src/content/certificates.js
+++ src/content/certificates.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react"
+import certificates from "./certificates.json"
 
 class Certificates extends Component {
     render() {
         return (
             <div id="certificates-container">
-
+                {certificates.map((cert, i) => 
+                    <div className="cert" key={i} onClick={() => window.open(cert.url, '_blank')}>
+                        <img className="thumbnail" src={cert.image} alt="thumbnail"/>
+                        <p className="name">{cert.name}</p>
+                        <a href={cert.url}>{cert.url}</a>
+                    </div>
+                )}
             </div>
         )
     }
